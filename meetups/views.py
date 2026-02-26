96252011775e22c3cc044ea55cd9d89b987e39d5
Amit Kumar Das <amit.das@sundewsolutions.com>
2026-02-26 14:47:05 +0530

    Key django template language features

--- meetups/views.py
+++ meetups/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "meetups/index.html")
+    meetups = [
+        { 'title': 'A First Meetup' },
+        { 'title': 'A Second Meetup' },
+    ]
+    return render(request, "meetups/index.html", {
+        'show_meetups': True,
+        'meetups': meetups
+    })
