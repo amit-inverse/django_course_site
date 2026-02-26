6c66a52ae0c380dc415b72b4e3878341753f45c8
Amit Kumar Das <amit.inverse@gmail.com>
2026-02-26 21:51:54 +0530

    Many-to-many relations

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Location, Meetup
+from .models import Location, Meetup, Participant
 
 # Register your models here.
 
@@ -10,3 +10,4 @@ class MeetupAdmin(admin.ModelAdmin):
 
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
